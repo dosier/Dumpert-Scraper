05bfaf164eade9479c67e78a25e89791f1087a26
stanbend <stanvdbend@gmail.com>
2019-05-10 00:29:51 +0200

    Used constant timeOut value and removed userAgent

--- src/main/kotlin/com/stan/scraper/page/PageGrab.kt
+++ src/main/kotlin/com/stan/scraper/page/PageGrab.kt
@@ -15,10 +15,9 @@ import java.util.concurrent.Callable
 class PageGrab<T>(private val url: URL, private val request: PageRequest<T>) : Callable<T> {
 
     override fun call(): T {
-
-        return request.parse( Jsoup.connect(url.toExternalForm())
-            .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36")
-            .timeout(60_000)
+        return request.parse(Jsoup
+            .connect(url.toExternalForm())
+            .timeout(TIMEOUT)
             .followRedirects(true)
             .get())
     }
