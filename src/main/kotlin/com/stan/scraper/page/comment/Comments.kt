f436538e92d26ae273b4d19c51310c9834ad8f72
stanbend <stanvdbend@gmail.com>
2019-05-10 00:57:16 +0200

    Added documentation to the Comments class

--- src/main/kotlin/com/stan/scraper/page/comment/Comments.kt
+++ src/main/kotlin/com/stan/scraper/page/comment/Comments.kt
@@ -3,7 +3,10 @@ package com.stan.scraper.page.comment
 import com.google.gson.annotations.Expose
 
 /**
- * TODO: add documentation
+ * This class represents a collection of comments of one comment page.
+ *
+ * @see [topComment] the top comment of the page.
+ * @see [comments] for all non-top comments of the page.
  *
  * @author  Stan van der Bend (https://www.rune-server.ee/members/StanDev/)
  * @since   2019-05-09
@@ -12,28 +15,54 @@ import com.google.gson.annotations.Expose
 class Comments {
 
     @Expose lateinit var topComment : Comment
+
     @Expose private val comments = ArrayList<Comment>()
 
+    /**
+     * Add a non-top comment to the [comments] list.
+     *
+     * @param comment the [Comment] to add to the list.
+     */
     fun add(comment: Comment){
         comments.add(comment)
     }
 
+    /**
+     * Sorts the [comments] list by [Comment.user] in a descending order.
+     */
     fun sortByUser(){
         comments.sortByDescending { it.user }
     }
 
+    /**
+     * Sorts the [comments] list by [Comment.content] in a descending order.
+     */
     fun sortByContent(){
         comments.sortByDescending { it.content }
     }
 
+    /**
+     * Sorts the [comments] list by [Comment.kudos] in a descending order.
+     */
     fun sortByKudos(){
         comments.sortByDescending { it.kudos }
     }
 
+    /**
+     * Sorts the [comments] list by [Comment.dateTime] in a descending order.
+     */
     fun sortByTime(){
         comments.sortByDescending { it.dateTime }
     }
 
+    /**
+     * Represents one comment on a dumper page.
+     *
+     * @param user      the username of the commenter.
+     * @param content   the content of the comment (text).
+     * @param dateTime  the time and date of when this comment was placed.
+     * @param kudos     the kudos score of this comment.
+     */
     class Comment(@Expose val user: String, @Expose val content : String, @Expose val dateTime : String, @Expose val kudos : Int)
 
 }
